5cf373a8178d587c4149ce9dbd76f27da4b28296
lmtrictu <50373590+lmtrictu@users.noreply.github.com>
2019-07-11 08:37:16 +0700

    Add files via upload

---
+++ PrintV7/PrintV7/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PrintV7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(408, 472);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+  //          this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+    }
+}
+
---
+++ PrintV7/PrintV7/Form1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrintV7
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            // string url = @"G:\C#-Optimus\print.html";
+
+            string[] passInArgs = Environment.GetCommandLineArgs();
+
+            int numberPrint = 1;
+            if (passInArgs.Length == 4)
+            {
+                numberPrint = Int16.Parse(passInArgs[3]);
+            }
+
+            string url = passInArgs[2];
+
+            foreach (string _passInArgs in passInArgs)
+            {
+                if (_passInArgs == "-u")     //kiểm tra sự tồn tại của -u
+                {
+                    for (int i = 1; i <= numberPrint; i++)
+                        PrintPage(url, 1);
+                }
+                else
+                    PrintPage(url, 0);
+            }
+            formClose.Interval = 10000;
+            formClose.Tick += new EventHandler(formClose_Tick);
+            formClose.Start();
+
+        }
+        Timer formClose = new Timer();
+        void formClose_Tick(object sender, EventArgs e) //set time close app
+        {
+            formClose.Stop();
+            formClose.Tick -= new EventHandler(formClose_Tick);
+            this.Close();
+        }
+
+
+        private void PrintPage(string url, int isPrint)
+        {
+            WebBrowser wb = new WebBrowser();
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(PrintDocument);
+            //wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+
+            // wb.Url = new Uri(url);
+            wb.Navigate(url);
+            if (isPrint == 0)
+            {
+                wb.Print();
+                wb.Dispose();
+            }
+            string keyName = @"Software\Microsoft\Internet Explorer\PageSetup";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("margin_bottom", "0");
+                    key.SetValue("margin_left", "0");
+                    key.SetValue("margin_right", "0");
+                    key.SetValue("margin_top", "0");
+
+                }
+            }
+        }
+        private void PrintDocument(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webBrowser1.ScrollBarsEnabled = false;
+            // webBrowser1.ScriptErrorsSuppressed = true;
+            // Print the document now that it is fully loaded.
+            ((WebBrowser)sender).Print();
+            ((WebBrowser)sender).ShowPageSetupDialog();
+
+            // Dispose the WebBrowser now that the task is complete. 
+            //((WebBrowser)sender).Dispose();
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            //webBrowser1.Print();
+            webBrowser1.ScrollBarsEnabled = false;
+            webBrowser1.ScriptErrorsSuppressed = true;
+            //webBrowser1.ShowPrintPreviewDialog();
+            // webBrowser1.Print();
+            // webBrowser1.Dispose();
+        }
+
+
+    }
+}
